962cf77aa7e9166b6969a4b0e61905b8e5aaa8b3
Avik-creator <avikm744@gmail.com>
2023-04-11 16:28:31 +0530

    Routes Learned

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home.dart';
+import 'package:flutter_catalog/pages/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => Homepage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }

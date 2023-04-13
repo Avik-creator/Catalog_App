788df96a8e5b6ba332d87173fdad24c0c5ff6e0f
Avik-creator <avikm744@gmail.com>
2023-04-13 16:50:54 +0530

    Learned Grid in Flutter (Half)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -40,13 +40,40 @@ class _HomepageState extends State<Homepage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20),
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items[index],
-                  );
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: GridTile(
+                        header: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.yellow,
+                            ),
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.black),
+                            )),
+                        child: Image.network(item.image),
+                        footer: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.yellow,
+                            ),
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.black),
+                            )),
+                      ));
                 },
+                itemCount: CatalogModel.items.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
